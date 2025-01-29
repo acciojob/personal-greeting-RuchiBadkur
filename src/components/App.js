28baffd43e31623da1c5f5e88a1676f35f4c6cb1
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2025-01-29 12:47:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,20 @@ import {useState} from "react";
 
 const App = () => {
    const [text, setText ] = useState("");
-    
-  const greet = () => {
-    // e.preventDefault();
-    const val = document.querySelector("input").value;
-    setText(val)
-    val === "" 
-      ? (setText(""), alert("Please enter your name")
-      )
-      : setText(`Hello ${val}!`)
-
-
-  }
+    const getVal = (e) => {
+       const val = e.target.value;
+       setText(val);
+    }
+   const clear = () => {
+      setText("");
+   }
   return (
     <div>
         {/* Do not remove the main div */}
       <h2>Enter your name:</h2>
-      <input type="text" id="text"/>
-      <button onClick={greet}>Submit</button>
-      <p>{text}</p>
+      <input type="text" id="text" onInput={getVal} value={text}/>
+      {!!text.length && <p>`Hello ${text}!`</p>}
+      <button onClick={clear}>clear</button>
     </div>
   )
 }
